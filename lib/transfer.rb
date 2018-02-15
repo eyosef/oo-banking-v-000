cf2dccc6c50b55bbcfc335af0de3993e8e6cd62c
Eden Mesfun <emesfun@gmail.com>
2018-02-15 14:03:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.sender.valid? && self.receiver.valid?
+    if self.sender.valid? && self.receiver.valid? && self.execute_transaction
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
       self.status = "reversed"
